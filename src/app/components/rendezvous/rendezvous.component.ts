b59ad8a50664c2cd096c795dcf6750fa81bf32d1
Ibtissem <ibtissem.ayachi@etudiant-enit.utm.tn>
2025-03-18 14:28:53 +0100

    rendez vous

--- src/app/components/rendezvous/rendezvous.component.ts
+++ src/app/components/rendezvous/rendezvous.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { RendezVousService } from '../../services/rendezvous.service';
+import { RouterModule } from '@angular/router';
+
+interface RendezVous {
+  idRDV: number;
+  date: string;
+  heure: string;
+  statut: string;
+  typeRendezVous: string;
+  IdPatient: number;
+  IdMedecin: number;
+}
 
 @Component({
   selector: 'app-rendezvous',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatTableModule, RouterModule],
   templateUrl: './rendezvous.component.html',
-  styleUrl: './rendezvous.component.css'
+  styleUrls: ['./rendezvous.component.css']
 })
-export class RendezvousComponent {
+export class RendezVousComponent implements OnInit {
+  rendezvous: RendezVous[] = [];
+  displayedColumns: string[] = ['date', 'heure', 'statut', 'typeRendezVous', 'IdPatient', 'IdMedecin'];
 
+  constructor(private rendezVousService: RendezVousService) {}
+
+  ngOnInit(): void {
+    this.loadRendezVous();
+  }
+
+  loadRendezVous() {
+    this.rendezVousService.getRendezVous().subscribe({
+      next: (data: RendezVous[]) => {
+        this.rendezvous = data;
+        console.log("Rendez-vous chargés :", this.rendezvous);
+      },
+      error: (err: unknown) => {
+        console.error("Erreur lors du chargement des rendez-vous :", err);
+      }
+    });
+  }
 }
--- src/app/services/rendezvous.service.ts
+++ src/app/services/rendezvous.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface RendezVous {
+  idRDV: number;
+  date: string;
+  heure: string;
+  statut: string;
+  typeRendezVous: string;
+  IdPatient: number;
+  IdMedecin: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class RendezvousService {
+export class RendezVousService {
+  private apiUrl = 'http://localhost:3000/api/rendezvous';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getRendezVous(): Observable<RendezVous[]> {
+    return this.http.get<RendezVous[]>(this.apiUrl);
+  }
 }
