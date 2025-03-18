64689adbdba749160f1307e03aa9b240852e2638
Ibtissem <ibtissem.ayachi@etudiant-enit.utm.tn>
2025-03-18 14:37:39 +0100

    conflits dans rendez vous

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,13 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { PatientsComponent } from './components/patients/patients.component';
 import { MedecinsComponent } from './components/medecins/medecins.component';
-import { RendezVousComponent } from './components/rendezvous/rendezvous.component';
+import { RendezvousComponent } from './components/rendezvous/rendezvous.component';
 
 export const routes: Routes = [
   { path: 'sidebar', component: SidebarComponent },
   { path: 'patients', component: PatientsComponent }, // ✅ Ajout de la route Patients
   { path: 'medecins', component: MedecinsComponent },
-  { path: 'rendezvous', component: RendezVousComponent },
+  { path: 'rendezvous', component: RendezvousComponent },
   { path: '', component: HomeComponent }, // Accueil par défaut
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
--- src/app/components/rendezvous/rendezvous.component.ts
+++ src/app/components/rendezvous/rendezvous.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource } from '@angular/material/table';
 import { RendezVousService } from '../../services/rendezvous.service';
 import { RouterModule } from '@angular/router';
 
@@ -21,9 +22,9 @@ interface RendezVous {
   templateUrl: './rendezvous.component.html',
   styleUrls: ['./rendezvous.component.css']
 })
-export class RendezVousComponent implements OnInit {
-  rendezvous: RendezVous[] = [];
+export class RendezvousComponent implements OnInit {
   displayedColumns: string[] = ['date', 'heure', 'statut', 'typeRendezVous', 'IdPatient', 'IdMedecin'];
+  dataSource = new MatTableDataSource<RendezVous>([]);
 
   constructor(private rendezVousService: RendezVousService) {}
 
@@ -34,8 +35,8 @@ export class RendezVousComponent implements OnInit {
   loadRendezVous() {
     this.rendezVousService.getRendezVous().subscribe({
       next: (data: RendezVous[]) => {
-        this.rendezvous = data;
-        console.log("Rendez-vous chargés :", this.rendezvous);
+        this.dataSource.data = data; // Mise à jour de `dataSource`
+        console.log("Rendez-vous chargés :", this.dataSource.data);
       },
       error: (err: unknown) => {
         console.error("Erreur lors du chargement des rendez-vous :", err);
